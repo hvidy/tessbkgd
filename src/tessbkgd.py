2189413bae51d904a6f0d38512eb8bebf05a153f
hvidy <tim.white.au@gmail.com>
2020-06-19 18:04:51 +1000

    minor adjustments

--- src/tessbkgd.py
+++ src/tessbkgd.py
@@ -57,20 +57,20 @@ class bkgd_tpf(lightkurve.TessTargetPixelFile):
 
 		return bkgd
 
-    @property
-    def flux(self):
-        """Returns the flux for all good-quality cadences."""
-        return self.hdu[1].data['FLUX'][self.quality_mask]
+	@property
+	def flux(self):
+		"""Returns the flux for all good-quality cadences."""
+		return self.hdu[1].data['FLUX'][self.quality_mask]
 
-    @flux.setter
-    def flux(self,value):
-        self.hdu[1].data['FLUX'][self.quality_mask] = value
+	@flux.setter
+	def flux(self,value):
+		self.hdu[1].data['FLUX'][self.quality_mask] = value
 
-    @property
-    def flux_bkg(self):
-        """Returns the background flux for all good-quality cadences"""
-        return self.hdu[1].data['FLUX_BKG'][self.quality_mask]
+	@property
+	def flux_bkg(self):
+		"""Returns the background flux for all good-quality cadences"""
+		return self.hdu[1].data['FLUX_BKG'][self.quality_mask]
 
-    @flux_bkg.setter
-    def flux_bkg(self,value):
-        self.hdu[1].data['FLUX_BKG'][self.quality_mask] = value
+	@flux_bkg.setter
+	def flux_bkg(self,value):
+		self.hdu[1].data['FLUX_BKG'][self.quality_mask] = value
